9025d1eb4e036a9780a23fbe8ad3cdda54a0de35
Oscar Paricio <oparicio@mobilemediacontent.com>
2024-10-27 19:20:47 +0100

    feat(AuthStore): Added Actions for Login, Logout, Get User and Reset State

--- stores/auth.ts
+++ stores/auth.ts
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 import type { Auth } from "~/models/auth/Auth.model";
+import type { User } from "~/models/auth/User";
 
 export const useAuth = defineStore("auth", {
     
@@ -23,10 +24,77 @@ export const useAuth = defineStore("auth", {
     actions:{
 
         // LOGIN
-        async login(email: string, password: string): Promise<void> {},
+        async login(email: string, password: string, redirect?: string): Promise<void> {
+
+            // Comprobamos si ya estamos logueados
+            if(this.logged && this.user) {
+                throw new Error("Ya estás logueado");
+            }
+
+            // Importamos las funciones necesarias
+            const router = useRouter();
+            const { $directus } = useNuxtApp() as any;
+
+            // Probamos a loguearnos
+            try {
+                
+                // Probamos a loguearnos
+                const login = await $directus.auth.login({
+                    email,
+                    password,
+                })
+
+                // Si el login es correcto, obtenemos los datos del usuario
+                await this.getUserData();
+
+                // Si tenemos una redirección, llamamos a la función de redirección
+                if(redirect) {
+                    router.push(redirect);
+                }
+
+
+            } catch (error) {
+                console.error(error);
+                throw new Error("Error al iniciar sesión");
+            }
+
+
+        },
 
         // LOGOUT
-        async logout(): Promise<void> {},
+        async logout(): Promise<void> {
+
+            // Validaciones
+            if(!this.logged) {
+                throw new Error("No estás logueado");
+            }
+
+            // Importamos las funciones necesarias
+            const router = useRouter();
+            const { $directus } = useNuxtApp() as any;
+
+            // Probamos a cerrar sesión
+            try {
+                
+                // Cerramos sesión
+                const logout = await $directus.auth.logout();
+
+                // Borramos cookie residual
+                const authExpiration = useCookie('auth_expires_at');
+                authExpiration.value = null;
+
+                // Actualizamos las propiedades de State
+                this.reset();
+
+                // Redirigimos al inicio
+                router.push('/');
+
+            } catch (error) {
+                console.error(error);
+                throw new Error("Error al cerrar sesión");
+            }
+
+        },
 
         // REGISTER
         async register(user: Auth["user"]): Promise<void> {},
@@ -35,7 +103,29 @@ export const useAuth = defineStore("auth", {
         async updateUser(user: Auth["user"]): Promise<void> {},
 
         // GET CURRENT USER
-        async getUserData(): Promise<void> {},
+        async getUserData(): Promise<void> {
+            
+            // Importamos las funciones necesarias
+            const { $directus } = useNuxtApp() as any;
+            
+            // Probamos a obtener los datos del usuario
+            try {
+                
+                // Obtenemos los datos del usuario
+                const user: User = await $directus.users.me.read({
+                    fields: ['*'],
+                });
+                
+                // Si la petición es correcta, actualizamos los datos del usuario
+                this.user   = user;
+                this.logged = true;
+
+            } catch (error) {
+                console.error(error);
+                throw new Error("Error al obtener los datos del usuario");
+            }
+
+        },
 
         // RESET
         async reset(): Promise<void> {
