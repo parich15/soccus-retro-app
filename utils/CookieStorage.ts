cd953ddf919a874ecb52c9d01f687f5b3f6e56be
Oscar Paricio <oparicio@mobilemediacontent.com>
2024-10-27 18:52:52 +0100

    feat(CookieStorage): Created cookie storage for Directus Plugins

---
+++ utils/CookieStorage.ts
@@ -0,0 +1,27 @@
+import type { CookieRef, NuxtApp  } from "#app";
+import type { AuthenticationData, AuthenticationStorage } from "@directus/sdk";
+
+// AUTHENTICATION STORAGE
+export class CookieStorage implements AuthenticationStorage {
+    
+    constructor(private nuxtApp: any) {}
+
+    cookieKey: string = 'auth';
+
+    get(): AuthenticationData | null {
+        const cookie = this.nuxtApp.runWithContext(() => useCookie(this.cookieKey)) as CookieRef<string | null>;
+        if(!cookie.value) return null;
+        return JSON.parse(useCookie(this.cookieKey).value!) as AuthenticationData;
+    }
+
+    set(data: AuthenticationData | null): void {
+        const cookie = this.nuxtApp.runWithContext(() => useCookie(this.cookieKey)) as CookieRef<string | null>;
+        cookie.value = JSON.stringify(data);
+    }
+
+    remove(): void {
+        const cookie = this.nuxtApp.runWithContext(() => useCookie(this.cookieKey)) as CookieRef<string | null>;
+        cookie.value = null;
+    }
+    
+}
